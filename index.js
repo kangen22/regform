d98c59e8a58c8d160e0e8c119640fa2b3449c4d4
kangen <kck.tema@gmail.com>
2024-09-10 17:09:04 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,73 @@
+const form = document.getElementById('form');
+const username = document.getElementById('username');
+const surname = document.getElementById('surname');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const password2 = document.getElementById('password2');
+
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    validateInputs();
+})
+
+const validateInputs = () => {
+    const usernameValue = username.value.trim();
+    const surnameValue = surname.value.trim();
+    const emailValue = email.value.trim();
+    const passwordValue = password.value.trim();
+    const password2Value = password2.value.trim();
+
+    if (usernameValue === '') {
+        setError(username, "Поле ім'я не може бути порожнім");
+    } else {
+        setSuccess(username);
+    }
+
+    if (surnameValue === '') {
+        setError(surname, "Поле прізвище не може бути порожнім");
+    } else {
+        setSuccess(surname);
+    }
+
+    if (emailValue === '') {
+        setError(email, `Поле пошта не може бути порожнім`);
+    } else {
+        setSuccess(email);
+    }
+
+    if (passwordValue === '') {
+        setError(password, "Поле пароль не може бути порожнім");
+    } else if (passwordValue.length < 6) {
+        setError(password, "Пароль має містити більше 6 цифр")
+    } else {
+        setSuccess(password);
+    }
+
+    if (password2Value === '') {
+        setError(password2, "Поле не може бути порожнім");
+    } else if (passwordValue !== password2Value) {
+        setError(password2, "Паролі не співпадають");
+    } else {
+        setSuccess(password2);
+    }
+}
+
+const setError = (input, message) => {
+    const inputControl = input.parentElement;
+    const errorDisplay = inputControl.querySelector('.error');
+
+    errorDisplay.innerText = message;
+    inputControl.classList.add('error');
+    inputControl.classList.remove('success');
+}
+
+const setSuccess = (input) => {
+    const inputControl = input.parentElement;
+    const errorDisplay = inputControl.querySelector('.error');
+
+    errorDisplay.innerText = '';
+    inputControl.classList.add('success');
+    inputControl.classList.remove('error');
+}
